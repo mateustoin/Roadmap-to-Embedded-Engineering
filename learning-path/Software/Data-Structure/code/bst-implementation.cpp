68b9919d11684d5ffd74fca14967f0f36f591b2c
Mateus Antonio <mateustoin@gmail.com>
2021-06-06 19:50:23 -0300

    feat: Delete operation on BST implemented

--- learning-path/Software/Data-Structure/code/bst-implementation.cpp
+++ learning-path/Software/Data-Structure/code/bst-implementation.cpp
@@ -13,6 +13,8 @@ struct BstNode {
 
 BstNode* GetNewNode(int data);
 BstNode* Insert(BstNode* root, int data);
+BstNode* Delete(BstNode* root, int data);
+BstNode* FindMin(BstNode* root);
 bool Search(BstNode* root, int data);
 void preorder(BstNode* root);
 void inorder(BstNode* root);
@@ -78,11 +80,56 @@ BstNode* Insert(BstNode* root, int data) {
     }
     // If the tree is not empty, test if data is bigger or smaller
     // to know if we'll create a left or right node in the subtree
-    else if(data <= root->data){
+    else if(data <= root->data) {
         root->left = Insert(root->left, data);
     }
     else{
         root->right = Insert(root->right, data);
+    }
+
+    return root;
+}
+
+BstNode* Delete(BstNode* root, int data) {
+    if (root == NULL) //Return empty tree
+        return root;
+    else if (data < root->data) //When you want to remove a data inside the left subtree
+        Delete(root->left, data);
+    else if (data > root->data) //When you want to remove a data inside the right subtree
+        Delete(root->right, data);
+    else{ //When the data is found
+        //Case 1: if the node have no children
+        if(root->left == NULL && root->right == NULL) {
+            delete root;
+            root = NULL;
+            return root;
+        }
+        //Case 2: node have only a child
+        else if (root->left == NULL) {
+            BstNode* temp = root;
+            root = root->right;
+            delete temp;
+            return root;
+        }
+        else if (root->right == NULL) {
+            BstNode* temp = root;
+            root = root->left;
+            delete temp;
+            return root;
+        }
+        //Case 3: node have two children
+        else {
+            BstNode* temp = FindMin(root->right);
+            root->data = temp->data;
+            root->right = Delete(root->right, temp->data);
+        }
+    }
+}
+
+BstNode* FindMin(BstNode* root) {
+    //Needed to find the min value node of the right subtree on delete function
+    while(root->left != NULL){
+        root = root->left;
     }
 
     return root;
